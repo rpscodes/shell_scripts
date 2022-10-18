bc324dc7e6f9bad4ea2e4cbba8ce8dd8508dfa45
Vamsi Ravula <83864467+rpscodes@users.noreply.github.com>
2022-10-18 12:44:01 -0700

    Update partner_api_calls_simulator.sh

--- partner_api_calls_simulator.sh
+++ partner_api_calls_simulator.sh
@@ -1,6 +1,14 @@
-for i in {1..10}
+for i in {1..25}
  do
     curl -v "https://traveldemo-istio-partner-product-3scale-apicast-staging.apps.cluster-ghmxk.ghmxk.sandbox140.opentlc.com/travels/Kiev?user_key=greensecret";
+ done
+
+for i in {1..20}
+ do
     curl -v "https://traveldemo-istio-partner-product-3scale-apicast-staging.apps.cluster-ghmxk.ghmxk.sandbox140.opentlc.com/travels/Kiev?user_key=bluesecret";
+ done
+ 
+ for i in {1..15}
+ do
     curl -v "https://traveldemo-istio-partner-product-3scale-apicast-staging.apps.cluster-ghmxk.ghmxk.sandbox140.opentlc.com/travels/Kiev?user_key=redsecret";
  done
